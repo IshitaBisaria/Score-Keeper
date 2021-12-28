ab294559b9e9cfb66608548dc7a5729ed3202a81
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2021-12-28 14:10:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+
+const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playto");
+let winningScore = 3;
+let isGameOver = false;
+
+const updateScores = function (player, opponent) {
+  if (!isGameOver) {
+    player.score++;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+    }
+    player.display.textContent = player.score;
+  }
+};
+
+const reset = function () {
+  isGameOver = false;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.button.disabled = false;
+    p.display.classList.remove("has-text-success", "has-text-danger");
+  }
+};
+
+p1.button.addEventListener("click", function () {
+  updateScores(p1, p2);
+});
+
+p2.button.addEventListener("click", function () {
+  updateScores(p2, p1);
+});
+
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetButton.addEventListener("click", reset);
